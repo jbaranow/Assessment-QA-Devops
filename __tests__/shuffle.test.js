30e4e92b2def6f277c38a3b22070c0d4517f8e3a
Julian Baranowski <julian.paul.baranowski@gmail.com>
2023-05-21 20:29:54 -0600

    shuffle testing in progress

--- __tests__/shuffle.test.js
+++ __tests__/shuffle.test.js
@@ -2,4 +2,18 @@ const shuffle = require("../src/shuffle");
 
 describe("shuffle should...", () => {
   // CODE HERE
+  test("return an array of the same length", () => {
+    const testArray = ["a", "b", "c"]
+    const result = shuffle(testArray)
+    expect(result.length).toEqual(testArray.length)
+  })
+
+  test("check that all same items are in array", () => {
+    const testArray = ["a", "b", "c"]
+    const result = shuffle(testArray)
+    expect(result).toContain("a")
+    expect(result).toContain("b")
+    expect(result).toContain("c")
+  })
+
 });

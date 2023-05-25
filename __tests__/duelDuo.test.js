19c362664176f5f75a2ab04c26625a06162346fd
Julian Baranowski <julian.paul.baranowski@gmail.com>
2023-05-24 20:57:58 -0600

    added automation tests

--- __tests__/duelDuo.test.js
+++ __tests__/duelDuo.test.js
@@ -15,4 +15,21 @@ describe("Duel Duo tests", () => {
     await driver.get("http://localhost:8000");
     await driver.wait(until.titleIs("Duel Duo"), 1000);
   }); 
+
+  test('Clicking Add to Duo displays #player-duo div', async () => {
+    await driver.get("http://localhost:8000")
+    const addToDuo = await driver.findElement(By.id('see-all')).click()
+    await driver.sleep(2000)
+    const playerDuo = await driver.findElement(By.id('all-bots'))
+    const displayed =  await playerDuo.isDisplayed()
+    expect(displayed).toBe(true)
+  })
+
+  test('Clicking Draw displays #choices div', async () => {
+    await driver.get("http://localhost:8000");
+    const draw = await driver.findElement(By.id('draw')).click()
+    const choices = await driver.findElement(By.id('choices'))
+    const displayed =  await choices.isDisplayed()
+    expect(displayed).toBe(true)
+  })
 });
